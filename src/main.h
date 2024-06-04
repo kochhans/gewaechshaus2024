93b8742de6efdd29a4e27cc502b280dbe6d7c56e
Hans <138814941+kochhans@users.noreply.github.com>
2024-06-04 09:24:19 +0200

    a

--- src/main.h
+++ src/main.h
@@ -46,18 +46,10 @@
 */
 
 //Pins neu:
-//EINGABEN
-#define SCHALTER 34 // Schalter Hand/Automatik
-#define S1AUF 26   // Taste
-#define S1ZU 25    // Taste
-#define S2AUF 33   // Taste
-#define S2ZU 35    // Taste
+
 
 //AUSGABEN
-#define MOTOR1PLUS 2 // Steuerung der H-Brücke
-#define MOTOR1MINUS 4
-#define MOTOR2PLUS 5
-#define MOTOR2MINUS 18
+
 
 #define OUTD0 2
 #define OUTD1 4
@@ -92,20 +84,13 @@
 /***********************************************************
     Variablen und Konstanten
 *************************************************************/
-const float tempAuf = 28.0;           // Bei dieser Temperatur: Fenster öffnen
-const float tempZu = 23.0;            // Bei dieser Temperatur: Fenster schließen
-const uint16_t schaltpause = 1000;    // minimale Pause zwischen Schaltvorgängen
-const uint16_t motordauerAuf = 1000;//45000; // 1000; // 45000 --alt:40000; // Dauer für Öffnungsvorgang
-const uint16_t motordauerZu = 2000;//50000;  // 1000;  // 50000- alt: 40000;  // Dauer für Schließvorgang in ms
-const uint16_t prellzeit = 500;
+
 const uint8_t displayaktiv = 1; // Soll das achtstellige 7-Segment-Display angesteuert werden? 0=nein, 1=ja
 boolean displayeinflag=false; // Flipflop, um das Display an und aus zu machen
 const int startPin = ONE_WIRE_BUS; //OneWire-Scan
 const int endPin = ONE_WIRE_BUS;
 
-uint8_t s1 = LOW;          // Logikzustand des Schalters
-uint8_t fensterstand1 = 0; // 0=zu, 1=offen
-uint8_t fensterstand2 = 0; // 0=zu, 1=offen
+
 
 float tempAktuell = 999; // Aktuelle Temperatur vom Sensor
 
@@ -126,10 +111,7 @@ uint8_t fctHandbetrieb();
 //SPI
 void fctLedOnboard(boolean);
 void fctSiebensegmentanzeige(uint8_t zustand, float messwertaktuell);
-void fct7SegWrite(uint8_t adresse, float messwertaktuell);
-void fct7SegAktiv(int adresse, boolean ein);
-void fct7SegHelligkeit(int adresse, int wert);
-void fct7SegLeeren(int adresse);
+
 
 //OneWire
 uint8_t fctFindOneWireDevices(int pin);
